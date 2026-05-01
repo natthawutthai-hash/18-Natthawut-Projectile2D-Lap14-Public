315671c8cff4e30f7bf884d0992722420ec1faf1
natthawut <natthawut.thai@bumail.net>
2026-05-01 23:11:06 +0700

    shdh

--- Assets/Scenes/S/butt.cs
+++ Assets/Scenes/S/butt.cs
@@ -1,16 +1,52 @@
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class butt : MonoBehaviour
+public class Shooter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private GameObject target;
+    [SerializeField] private GameObject bulletPrefab;
+
     void Start()
     {
-        
+        GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //Vector2 screenPos = Mouse.current.position.ReadValue();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 5f, Color.red, 5f);
+
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+            if (hit.collider != null)
+            {
+                target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                Debug.Log(hit.collider.name);
+
+                Vector2 projectileVelocity = CalculateProjectileVelocity(shootPoint.position, hit.point, 1f);
+
+                Rigidbody2D shootBuller = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity).GetComponent<Rigidbody2D>();
+
+                shootBuller.linearVelocity = projectileVelocity;
+
+            }
+        }
+    }
+    Vector2 CalculateProjectileVelocity(Vector2 origin, Vector2 target, float time)
+    {
+        Vector2 distance = target - origin;
+
+        float velocityX = distance.x / time;
+        float velocityY = distance.y / time + 0.5f * Mathf.Abs(Physics2D.gravity.y) * time;
+
+        Vector2 projectileVelocity = new Vector2(velocityX, velocityY);
+
+        return projectileVelocity;
     }
 }
